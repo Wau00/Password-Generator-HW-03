0a8c01a86ac467f55b0120d17bb9419f375f1d5e
walterunderwood <walonsounderwood@gmail.com>
2022-03-31 19:51:41 -0700

    First prompt complete, (How many characters?) and conditions were added

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,3 +13,14 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+  var passLength = prompt("How many characters your password will have? (Minimum 8 characters).")
+  if (passLength < 8) {
+    alert("Your password must be minimum of 8 characters.");
+  } else if (passLength > 128) {
+    alert("Oh, I forgot! Your password must not exceed the 128 characters.");
+   } 
+ }
+ 
+
