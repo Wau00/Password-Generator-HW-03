35e33affcd27478293ece9a91cc308a79e10d05f
walterunderwood <walonsounderwood@gmail.com>
2022-03-31 20:48:20 -0700

    Setting the variables: Lowercase, Uppercase, Numbers, Special symbols

--- Assets/script.js
+++ Assets/script.js
@@ -2,6 +2,18 @@
 var generateBtn = document.querySelector("#generate");
 
 
+// Array of lowercase letters
+passLowCase: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+// Array of UPPERCASE letters
+passUppCase: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+// Array of Numbers
+passNumCase: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+
+// Array of Symbols
+passSymCase: ["?","!","<",">","=","@","%","$", ")","_","&","#"]
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -15,6 +27,7 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
+  
   var passLength = prompt("How many characters your password will have? (Minimum 8 characters).")
   if (passLength < 8) {
     alert("Your password must be minimum of 8 characters.");
